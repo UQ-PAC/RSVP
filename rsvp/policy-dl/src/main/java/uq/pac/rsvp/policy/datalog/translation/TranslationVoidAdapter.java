8751752a531dc200a5b2a73fb705a8285f4f7906
Kostyantyn Vorobyov <k.vorobyov@uq.edu.au>
2026-02-25 16:19:49 +1000

    policy-dl: refactoring adapters

--- rsvp/policy-dl/src/main/java/uq/pac/rsvp/policy/datalog/translation/TranslationVoidAdapter.java
+++ rsvp/policy-dl/src/main/java/uq/pac/rsvp/policy/datalog/translation/TranslationVoidAdapter.java
@@ -1,15 +1,11 @@
 package uq.pac.rsvp.policy.datalog.translation;
 
-import uq.pac.rsvp.policy.ast.Policy;
-import uq.pac.rsvp.policy.ast.PolicySet;
-import uq.pac.rsvp.policy.ast.expr.*;
-import uq.pac.rsvp.policy.ast.visitor.PolicyVisitor;
 import uq.pac.rsvp.policy.datalog.ast.DLRuleExpr;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class TranslationVoidAdapter implements PolicyVisitor {
+public abstract class TranslationVoidAdapter extends VoidVisitorAdapter {
     protected TranslationSchema schema;
     protected TranslationTyping typing;
     protected List<DLRuleExpr> expressions;
@@ -20,83 +16,7 @@ public abstract class TranslationVoidAdapter implements PolicyVisitor {
         this.expressions = new ArrayList<>();
     }
 
-    @Override
-    public void visitPolicySet(PolicySet policySet) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitPolicy(Policy policy) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitBinaryExpr(BinaryExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitPropertyAccessExpr(PropertyAccessExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitCallExpr(CallExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitConditionalExpr(ConditionalExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitRecordExpr(RecordExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitSetExpr(SetExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitUnaryExpr(UnaryExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitVariableExpr(VariableExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitBooleanExpr(BooleanExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitEntityExpr(EntityExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitLongExpr(LongExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitSlotExpr(SlotExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitStringExpr(StringExpression expr) {
-        throw new RuntimeException("unsupported");
-    }
-
-    @Override
-    public void visitTypeExpr(TypeExpression expr) {
-        throw new RuntimeException("unsupported");
+    public List<DLRuleExpr> getExpressions() {
+        return expressions;
     }
 }

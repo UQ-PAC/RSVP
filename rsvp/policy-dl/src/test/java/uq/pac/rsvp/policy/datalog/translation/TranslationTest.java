3a6ac055f8d8a5743889d7c1ce0ea7f02c08b7a0
Kostyantyn Vorobyov <k.vorobyov@uq.edu.au>
2026-03-04 11:46:32 +1000

    policy-dl: fix test failires

--- rsvp/policy-dl/src/test/java/uq/pac/rsvp/policy/datalog/translation/TranslationTest.java
+++ rsvp/policy-dl/src/test/java/uq/pac/rsvp/policy/datalog/translation/TranslationTest.java
@@ -3,6 +3,7 @@ package uq.pac.rsvp.policy.datalog.translation;
 import com.cedarpolicy.model.entity.Entities;
 import com.cedarpolicy.model.exception.AuthException;
 import com.cedarpolicy.model.exception.InternalException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import uq.pac.rsvp.policy.ast.Policy;
 import uq.pac.rsvp.policy.ast.PolicySet;
@@ -11,53 +12,39 @@ import uq.pac.rsvp.policy.ast.schema.Schema;
 import uq.pac.rsvp.policy.datalog.ast.*;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uq.pac.rsvp.policy.datalog.DLTest.pathOf;
 
 public class TranslationTest {
 
-    private static final Path ENTITIES = Path.of("examples/photoapp/entities.json");
-    private static final Path POLICIES = Path.of("examples/photoapp/policy.cedar");
-    private static final Path SCHEMA = Path.of("examples/photoapp/schema.cedarschema");
+    private static final Path ENTITIES = pathOf("photoapp/entities.json");
+    private static final Path POLICIES = pathOf("photoapp/policy.cedar");
+    private static final Path SCHEMA = pathOf("photoapp/schema.cedarschema");
 
     Map<String, Policy> getPolicies() throws IOException, InternalException {
         Map<String, Policy> policies = new HashMap<>();
         PolicySet policySet = PolicySet.parseCedarPolicySet(POLICIES);
         for (Policy p : policySet) {
             Map<String, String> annotations = p.getAnnotations();
-            assertEquals(1, annotations.size());
-            String annotation = annotations.keySet().stream().findFirst().orElse(null);
-            if (!annotations.get(annotation).equalsIgnoreCase("unsupported")) {
-                policies.put(annotation, p);
+            // All annotations should be named
+            assert (annotations.containsKey("name"));
+            String name = annotations.get("name");
+            // Names should be unique
+            assert(!name.isEmpty() && !policies.containsKey(name));
+            if (annotations.containsKey("skip")) {
+                System.out.println("Skipping  policy: " + name);
+            } else {
+                policies.put(name, p);
             }
         }
         return policies;
     }
 
     @Test
-    public void test() throws IOException, InternalException {
-        Entities entities = Entities.parse(ENTITIES);
-        Schema schema = Schema.parseCedarSchema(SCHEMA);
-        TranslationSchema translationSchema = new TranslationSchema(schema);
-
-        TranslationTyping types = new TranslationTyping(schema);
-        Map<String, Policy> policies = getPolicies();
-
-        policies.forEach((annotation, policy) -> {
-            DLRuleDecl declaration = new DLRuleDecl(annotation,
-                    new DLDeclTerm("principal", DLType.SYMBOL),
-                    new DLDeclTerm("resource", DLType.SYMBOL),
-                    new DLDeclTerm("action", DLType.SYMBOL));
-
-            List<List<Expression>> disjunctions = NFConverter.toDNF(policy.getCondition());
-            for (List<Expression> disjunction : disjunctions) {
-                DLRule rule = TranslationVisitor.translate(translationSchema, disjunction, declaration);
-            }
-        });
-    }
-
     void TranslationDriverTest() throws IOException, AuthException {
         DLProgram program = Translation.translate(SCHEMA, POLICIES, ENTITIES);
     }

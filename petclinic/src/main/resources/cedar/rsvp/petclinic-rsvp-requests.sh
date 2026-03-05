d69cdad5528b4cc9b74b31f045d0c63c8c32925b
nunesgh <contact@nunesgh.com>
2026-03-05 21:26:05 +1100

    Refactors petclinic-rsvp-requests.sh and petclinic-original-requests.sh.

--- petclinic/src/main/resources/cedar/rsvp/petclinic-rsvp-requests.sh
+++ petclinic/src/main/resources/cedar/rsvp/petclinic-rsvp-requests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ERROR=
 ENTITIES=petclinic-rsvp-entities.json
@@ -11,11 +11,11 @@ validate() {
         --policies $POLICY)
     
     echo $out
-    echo -e "\n"
 }
 
 authorise() {
     echo "$1"
+
     out=$(cedar authorize \
         --policies $POLICY \
         --schema $SCHEMA \
@@ -24,31 +24,40 @@ authorise() {
 
     out=$(echo $out)
 
-    if [ "$out" == "$2" ]; then
-        echo -e "\e[32m OK [ "Expected: $out". ] \e[0m"
+    if [[ "$out" == "$2" ]]; then
+        echo -e "\e[1;32m OK [ "Expected: $2. Received: $out". ] \e[0m"
     else
-        echo -e "\e[31m FAIL [ "Expected: $2, got $out". ] \e[0m"
+        echo -e "\e[1;31m FAIL [ "Expected: $2. Received: $out". ] \e[0m"
+        ERROR=1
     fi
-    echo -e "\n"
 }
 
-echo "Schema Validation."
+echo -e "\e[1;34m Schema Validation. \e[0m"
 validate
+echo -e "\n"
 
-echo "ALLOW Tests."
-for file in "./ALLOW"/*
-do
-    authorise $file ALLOW
-done
-
-echo "DENY Tests."
-for file in "./DENY"/*
-do
-    authorise $file DENY
-done
-
-if [ -n "$ERROR" ]; then
-    echo -e "\e[31m ERROR: There were errors! \e[0m"
+if [[ -d "./ALLOW" ]]; then
+    echo -e "\e[1;34m ALLOW Tests. \e[0m"
+    for file in "./ALLOW"/*; do
+        authorise $file ALLOW
+    done
+    echo -e "\n"
 else
-    echo -e "\e[32m ALL OK! \e[0m"
+    echo -e "\e[1;34m No ALLOW tests defined. \n\e[0m"
+fi
+
+if [[ -d "./DENY" ]]; then
+    echo -e "\e[1;34m DENY Tests. \e[0m"
+    for file in "./DENY"/*; do
+        authorise $file DENY
+    done
+    echo -e "\n"
+else
+    echo -e "\e[1;34m No DENY tests defined. \n\e[0m"
+fi
+
+if [[ -n "$ERROR" ]]; then
+    echo -e "\e[1;31m ERROR: There were failed tests! \e[0m"
+else
+    echo -e "\e[1;32m ALL OK! \e[0m"
 fi

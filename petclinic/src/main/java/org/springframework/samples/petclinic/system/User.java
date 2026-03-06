d8bb650bd0b7e12db6e146f9b6a516261b68c32b
nunesgh <contact@nunesgh.com>
2026-03-06 20:11:49 +1100

    Adds HttpSession-based user switcher. Includes both hard-coded users (Administrator and Guest) and dynamic users from the database (Veterinarians).

---
+++ petclinic/src/main/java/org/springframework/samples/petclinic/system/User.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.system;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- petclinic/src/main/resources/db/h2/data.sql
+++ petclinic/src/main/resources/db/h2/data.sql
@@ -51,3 +51,7 @@ INSERT INTO visits VALUES (default, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (default, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits VALUES (default, 8, '2013-01-03', 'neutered');
 INSERT INTO visits VALUES (default, 7, '2013-01-04', 'spayed');
+
+INSERT INTO users VALUES (default, 'James Carter');
+INSERT INTO users VALUES (default, 'Linda Douglas');
+INSERT INTO users VALUES (default, 'Sharon Jenkins');

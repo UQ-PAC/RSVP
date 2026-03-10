d129e495339f6bbfda6281cb709f6232633e9880
nunesgh <contact@nunesgh.com>
2026-03-10 11:28:59 +1100

    Removes leftovers from overhaul of H2 database and related files.

--- petclinic/src/main/java/org/springframework/samples/petclinic/owner/PetType.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/owner/PetType.java
@@ -15,8 +15,6 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-// import org.springframework.samples.petclinic.model.NamedEntity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 

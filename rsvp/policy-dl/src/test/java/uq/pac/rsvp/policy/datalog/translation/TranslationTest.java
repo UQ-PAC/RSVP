3f864fd52122a4bec12191fb3e3f7c4dbcdd3d15
Kostyantyn Vorobyov <k.vorobyov@uq.edu.au>
2026-03-16 14:41:43 +1000

    policy-dl: set test case

--- rsvp/policy-dl/src/test/java/uq/pac/rsvp/policy/datalog/translation/TranslationTest.java
+++ rsvp/policy-dl/src/test/java/uq/pac/rsvp/policy/datalog/translation/TranslationTest.java
@@ -78,7 +78,7 @@ public class TranslationTest {
         }
     }
 
-    private final static String ONE_OFF = "enum";
+    private final static String ONE_OFF = "set";
 
     // Running tests for one directory separately for no particular
     // reason apart from being able to launch it separately

e17615567a62306e90c96fc47a88e2216fbde9ae
nunesgh <contact@nunesgh.com>
2026-03-20 17:10:02 +1100

    Merges petclinic-rsvp-requests.sh and petclinic-rsvp-requests-verbose.sh. Introduces optional flags. Updates README file with new instructions.

--- petclinic/src/main/resources/cedar/rsvp/petclinic-rsvp-requests.sh
+++ petclinic/src/main/resources/cedar/rsvp/petclinic-rsvp-requests.sh
@@ -1,9 +1,42 @@
 #!/usr/bin/env bash
 
 ERROR=
-SCHEMA=petclinic-rsvp-schema.cedarschema
-ENTITIES=petclinic-rsvp-entities.json
-POLICY="${1:-petclinic-rsvp-policy.cedar}"
+SCHEMA="petclinic-rsvp-schema.cedarschema"
+ENTITIES="petclinic-rsvp-entities.json"
+POLICY="petclinic-rsvp-policy.cedar"
+
+ALLOW="./ALLOW"
+DENY="./DENY"
+VERBOSE=false
+
+while getopts "htvp:" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 [-h] [-t] [-v] [-p another-policy.cedar]" >&2
+      exit 0
+      ;;
+    t)
+      ALLOW="./ALLOW-TEST"
+      DENY="./DENY-TEST"
+      ;;
+    v)
+      VERBOSE=true
+      ;;
+    p)
+      POLICY="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND -1))
 
 validate() {
     out=$(cedar validate \
@@ -15,21 +48,49 @@ validate() {
 
 authorise() {
     echo "$1"
-    cat "$1"
+    if [[ "$VERBOSE" == true ]]; then
+        cat "$1"
+    fi
 
-    out=$(cedar authorize \
-        --policies $POLICY \
-        --schema $SCHEMA \
-        --entities $ENTITIES \
-        --request-json $1)
+    mapfile -t out < <(cedar authorize \
+                        --verbose \
+                        --policies $POLICY \
+                        --schema $SCHEMA \
+                        --entities $ENTITIES \
+                        --request-json $1 \
+                        | sed -e 's/\r//g' \
+                        -e 's/^[[:space:]]*//' \
+                        -e 's/[[:space:]]*$//')
 
-    out=$(echo $out)
+    declare -a result=()
 
-    if [[ "$out" == "$2" ]]; then
-        echo -e "\e[1;32m OK [ "Expected: $2. Received: $out". ] \e[0m"
+    for entry in "${out[@]}"; do
+        if [[ "$entry" =~ ^policy[[:digit:]]+$|^ALLOW$|^DENY$|^Policy:.*\.$ ]]; then
+            temp=$(sed -e 's/^Policy\:[[:space:]]/\"/' -e 's/\.$/\"/' <<< "$entry")
+            result+=("$temp")
+        fi
+    done
+
+    # for index in "${!result[@]}"; do
+    #     printf "[%d]: %s\n" "$index" "${result[$index]}"
+    # done
+
+    if [[ "${result[0]}" == "$2" ]]; then
+        echo -e "\e[1;32m OK [ "Expected: $2. Received: ${result[0]}". ] \e[0m"
     else
-        echo -e "\e[1;31m FAIL [ "Expected: $2. Received: $out". ] \e[0m"
+        echo -e "\e[1;31m FAIL [ "Expected: $2. Received: ${result[0]}". ] \e[0m"
         ERROR=1
+    fi
+
+    total="${#result[@]}"
+
+    if [[ "$total" == "1" ]]; then
+        printf "%b No policies applied! Decision based on Cedar's default DENY. %b\n" "\033[1;34m" "\033[0m"
+    else
+        original_ifs="$IFS"
+        IFS=";"
+        printf "%b Decision based on policies: %s. %b\n" "\033[1;34m" "${result[*]:1}" "\033[0m"
+        IFS="$original_ifs"
     fi
 }
 
@@ -37,9 +98,9 @@ echo -e "\e[1;34m Schema Validation. \e[0m"
 validate
 echo -e "\n"
 
-if [[ -d "./ALLOW" ]]; then
+if [[ -d "$ALLOW" ]]; then
     echo -e "\e[1;34m ALLOW Tests. \e[0m"
-    for file in "./ALLOW"/*; do
+    for file in "$ALLOW"/*; do
         authorise $file ALLOW
     done
     echo -e "\n"
@@ -47,9 +108,9 @@ else
     echo -e "\e[1;34m No ALLOW tests defined. \n\e[0m"
 fi
 
-if [[ -d "./DENY" ]]; then
+if [[ -d "$DENY" ]]; then
     echo -e "\e[1;34m DENY Tests. \e[0m"
-    for file in "./DENY"/*; do
+    for file in "$DENY"/*; do
         authorise $file DENY
     done
     echo -e "\n"

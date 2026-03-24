370b3527119a1f0397450a613433275829155b05
Rebecca O'Donoghue <uqroydon@uq.edu.au>
2026-03-24 12:44:12 +1000

    report fixes

--- rsvp/app/src/main/java/uq/pac/rsvp/Verification.java
+++ rsvp/app/src/main/java/uq/pac/rsvp/Verification.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import uq.pac.rsvp.policy.ast.Policy;
 import uq.pac.rsvp.policy.ast.PolicySet;
 import uq.pac.rsvp.policy.ast.schema.Schema;
+import uq.pac.rsvp.support.SourceLoc;
 import uq.pac.rsvp.support.reporting.Report;
 import uq.pac.rsvp.support.reporting.Report.Severity;
 
@@ -27,12 +28,24 @@ public class Verification {
 
         Random r = new Random();
 
+        SourceLoc[] locations = new SourceLoc[2];
+        int i = 0;
+
         for (Policy policy : policies) {
             int num = r.nextInt(100);
+            int detail = r.nextInt(100);
+            if (i < 2) {
+                locations[i] = policy.getSourceLoc();
+            }
+
             reports.add(
 
                     new Report(num < 34 ? Severity.Info : num < 67 ? Severity.Warning : Severity.Error,
-                            "This is a fantastic policy. Well done Angus.", policy.getSourceLoc()));
+                            "This is a fantastic policy. Well done Angus.",
+                            detail < 50 ? ""
+                                    : "This is a very detailed report on your policy. Your policy is truly a thing of beauty, and we should all aspire to emulate this policy.",
+                            policy.getSourceLoc(), i == 2 ? locations : new SourceLoc[0]));
+            i++;
         }
 
         return reports;

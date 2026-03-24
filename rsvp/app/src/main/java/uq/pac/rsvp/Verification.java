153ef0e4f865736d27ba0ef4471fb76e63ecc7d8
Rebecca O'Donoghue <uqroydon@uq.edu.au>
2026-03-24 11:26:28 +1000

    extend report

--- rsvp/app/src/main/java/uq/pac/rsvp/Verification.java
+++ rsvp/app/src/main/java/uq/pac/rsvp/Verification.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
 import uq.pac.rsvp.policy.ast.Policy;
 import uq.pac.rsvp.policy.ast.PolicySet;
 import uq.pac.rsvp.policy.ast.schema.Schema;
@@ -30,14 +29,12 @@ public class Verification {
 
         for (Policy policy : policies) {
             int num = r.nextInt(100);
-            System.err.println(policy.getName());
             reports.add(
 
                     new Report(num < 34 ? Severity.Info : num < 67 ? Severity.Warning : Severity.Error,
                             "This is a fantastic policy. Well done Angus.", policy.getSourceLoc()));
         }
 
-        // Gson gson = new Gson();
         return reports;
     }
 

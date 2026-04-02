c84f124bbc6d73a7950a26343ec982e59924d4d3
nunesgh <contact@nunesgh.com>
2026-04-02 18:17:43 +1100

    Adds users roles to user switcher.

---
+++ petclinic/src/main/java/org/springframework/samples/petclinic/system/Role.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.system;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+	@Id
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "name")
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}

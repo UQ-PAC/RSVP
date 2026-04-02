7795458bedf7bba7683580bae75fd8d03ede85ad
nunesgh <contact@nunesgh.com>
2026-04-02 18:17:43 +1100

    Adds users roles to user switcher.

--- petclinic/src/main/java/org/springframework/samples/petclinic/system/User.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/system/User.java
@@ -1,15 +1,37 @@
 package org.springframework.samples.petclinic.system;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Table;
+import jakarta.persistence.ManyToMany;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
 
 	@Id
+	private Integer entity_id;
+
 	private String username;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+
+	public Integer getId() {
+		return entity_id;
+	}
+
+	public void setId(Integer entity_id) {
+		this.entity_id = entity_id;
+	}
 
 	public String getUsername() {
 		return username;
@@ -17,6 +39,21 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getRoleDescriptions() {
+		if (this.roles == null || this.roles.isEmpty()) {
+			return "No Additional Details";
+		}
+		return this.roles.stream().map(Role::getName).collect(Collectors.joining(", "));
 	}
 
 }

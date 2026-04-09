7efd37cb0b7a9933979c8ce21c740b3c6e3b70ea
nunesgh <contact@nunesgh.com>
2026-04-09 10:55:29 +1000

    Adds users clinics to user switcher.

--- petclinic/src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.system;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String> {
-
+    Optional<User> findByUsername(String username);
 }

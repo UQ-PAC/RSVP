5456ee7f0f106ac081d2e7a58f3e2098d6e2a7ed
nunesgh <contact@nunesgh.com>
2026-04-09 17:12:15 +1000

    GlobalModelAttributes.java: Fixes empty user-switcher on error pages.

--- petclinic/src/main/java/org/springframework/samples/petclinic/user/GlobalModelAttributes.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/user/GlobalModelAttributes.java
@@ -1,9 +1,11 @@
 package org.springframework.samples.petclinic.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.samples.petclinic.system.User;
 import org.springframework.samples.petclinic.system.UserRepository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -16,25 +18,30 @@ public class GlobalModelAttributes {
 		this.userRepository = userRepository;
 	}
 
-	@ModelAttribute("currentUser")
-	public User populateUser(HttpSession session) {
-		String sessionUser = (String) session.getAttribute("currentUser");
+	@ModelAttribute
+    public void populateLayoutData(HttpSession session, HttpServletRequest request, Model model) {
+        String sessionUser = (String) session.getAttribute("currentUser");
 
-		final String activeUsername = (sessionUser != null) ? sessionUser : "Guest";
+        final String activeUsername = (sessionUser != null) ? sessionUser : "Guest";
 
 		System.out.println("activeUsername: " + activeUsername);
 
-		return this.userRepository.findByUsername(activeUsername)
-			.orElseGet(() -> {
-				User transientUser = new User();
-				transientUser.setUsername(activeUsername);
-				return transientUser;
-			});
-	}
+        User currentUser = this.userRepository.findByUsername(activeUsername)
+            .orElseGet(() -> {
+                User transientUser = new User();
+                transientUser.setUsername(activeUsername); 
+                return transientUser;
+            });
 
-	@ModelAttribute("dynamicUsers")
-	public Iterable<User> populateDynamicUsers() {
-		return this.userRepository.findAll();
-	}
+        Iterable<User> dynamicUsers = this.userRepository.findAll();
+
+        // 1. Populate the Model for standard view rendering
+        model.addAttribute("currentUser", currentUser);
+        model.addAttribute("dynamicUsers", dynamicUsers);
+
+        // 2. Populate the Request Attributes to persist through exceptions and /error forwards
+        request.setAttribute("currentUser", currentUser);
+        request.setAttribute("dynamicUsers", dynamicUsers);
+    }
 
 }

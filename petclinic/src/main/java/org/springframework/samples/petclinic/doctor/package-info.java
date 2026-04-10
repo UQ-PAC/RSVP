b509551e26db1f673983e6396753681cbd58d227
nunesgh <contact@nunesgh.com>
2026-04-10 18:02:22 +1000

    PetClinic to ChildClinic: Renames Java folders.

--- petclinic/src/main/java/org/springframework/samples/petclinic/doctor/package-info.java
+++ petclinic/src/main/java/org/springframework/samples/petclinic/doctor/package-info.java
@@ -13,27 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.springframework.samples.petclinic.owner;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-/**
- * Repository class for <code>Gender</code> domain objects.
- *
- * @author Patrick Baumgartner
- */
-
-public interface GenderRepository extends JpaRepository<Gender, Integer> {
-
-	/**
-	 * Retrieve all {@link Gender}s from the data store.
-	 * @return a Collection of {@link Gender}s.
-	 */
-	@Query("SELECT gender FROM Gender gender ORDER BY gender.name")
-	List<Gender> findGenders();
-
-}
+package org.springframework.samples.petclinic.doctor;

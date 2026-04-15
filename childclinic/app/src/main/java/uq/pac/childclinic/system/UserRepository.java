f9f837aed7929d9d2767a50caf789c46eb383b67
nunesgh <contact@nunesgh.com>
2026-04-15 15:13:39 +1000

    system/UserRepository.java: Fixes type.

--- childclinic/app/src/main/java/uq/pac/childclinic/system/UserRepository.java
+++ childclinic/app/src/main/java/uq/pac/childclinic/system/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 	Optional<User> findByUsername(String username);
 

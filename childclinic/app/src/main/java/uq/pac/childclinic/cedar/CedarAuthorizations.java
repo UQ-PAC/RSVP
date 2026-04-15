b15ea09ff95cf643a855ad588d47507ce036f731
nunesgh <contact@nunesgh.com>
2026-04-15 10:12:46 +1000

    Implements multiple Cedar authorizations.

--- childclinic/app/src/main/java/uq/pac/childclinic/cedar/CedarAuthorizations.java
+++ childclinic/app/src/main/java/uq/pac/childclinic/cedar/CedarAuthorizations.java
@@ -1,22 +1,12 @@
 package uq.pac.childclinic.cedar;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Repeatable(CedarAuthorizations.class)
-public @interface CedarAuthorization {
-
-	String action();
-
-	String resourceType();
-
-	String resourceId() default "";
-
-	boolean validate() default true;
-
+public @interface CedarAuthorizations {
+    CedarAuthorization[] value();
 }

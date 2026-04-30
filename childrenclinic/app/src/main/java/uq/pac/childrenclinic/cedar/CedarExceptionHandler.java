48a60609ed7c3afab6cec2e54f0458fab3b67385
nunesgh <contact@nunesgh.com>
2026-05-01 09:59:23 +1000

    Updates error handling. Also adds CSS file to repository.

--- childrenclinic/app/src/main/java/uq/pac/childrenclinic/cedar/CedarExceptionHandler.java
+++ childrenclinic/app/src/main/java/uq/pac/childrenclinic/cedar/CedarExceptionHandler.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @ControllerAdvice
 public class CedarExceptionHandler {
+
+	private final CedarLogContext cedarLogContext;
+
+	public CedarExceptionHandler(CedarLogContext cedarLogContext) {
+		this.cedarLogContext = cedarLogContext;
+	}
 
 	@ExceptionHandler(CedarDeniedException.class)
 	@ResponseStatus(HttpStatus.FORBIDDEN)
@@ -15,6 +23,11 @@ public class CedarExceptionHandler {
 		ModelAndView modelAndView = new ModelAndView("error");
 		modelAndView.addObject("message", exception.getMessage());
 		modelAndView.addObject("status", 403);
+
+		List<String> logs = this.cedarLogContext.getLogs();
+		modelAndView.addObject("cedarLogs", logs);
+		this.cedarLogContext.clearLogs();
+
 		return modelAndView;
 	}
 

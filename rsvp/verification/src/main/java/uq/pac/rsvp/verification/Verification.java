5d20a429b1ad69ab46b14c2806d27252afa033f3
Davin McCall <davmac@davmac.org>
2026-05-05 15:57:02 +1000

    verification: move testing code to standalone if block
    Put normal code outside "if (TESTING)" conditional so it's not unduly
    indented.

--- rsvp/verification/src/main/java/uq/pac/rsvp/verification/Verification.java
+++ rsvp/verification/src/main/java/uq/pac/rsvp/verification/Verification.java
@@ -67,187 +67,185 @@ public class Verification {
     }
 
     public static Set<Report> verifyPolicies(Set<List<Pair<String, Path>>> policies, Map<String, Path> schemas, Map<String, Path> entities, Map<String, Path> invariants) throws RsvpException, IOException, ConfigurationException, InterruptedException, IllegalAccessException {
-        if (!TESTING) {
-            // TODO: use PolicyParser instead of PolicyProgram and pass file name separately
-            if (policies.isEmpty() || policies.iterator().next().isEmpty()) {
-                throw new ConfigurationException("No policies provided");
-            }
-
-            if (schemas.isEmpty()) {
-                throw new ConfigurationException("No schema provided");
-            }
-
-            if (entities.isEmpty()) {
-                throw new ConfigurationException("No entities provided");
-            }
-
-            Set<Report> results = new HashSet<>();
-
-            List<Pair<String, Path>> policyVersions = policies.iterator().next();
-            Path policiesPath = policyVersions.getLast().getValue();
-            Path schemaPath = schemas.values().iterator().next();
-            Path entitiesPath = entities.values().iterator().next();
-            Path invariantsPath = invariants.values().iterator().next();
-
-            Path dlPath = Files.createTempDirectory("rsvp-");
-
-            Translation translation = new Translation(schemaPath, policiesPath, entitiesPath, invariantsPath, dlPath);
-
-            Map<Policy, RequestSet> policyResults = translation.getPolicyResult();
-
-            // Generate the inverse map, request -> policy set:
-            Map<Request, Set<Policy>> requestPolicyMap = new HashMap<>();
-            policyResults.forEach((k, v) -> {
-                v.forEach(r -> {
-                    requestPolicyMap.computeIfAbsent(r, y -> {
-                        return new HashSet<>();
-                    }).add(k);
-                });
-            });
-
-            class PolicyAnalysisResult {
-                // Whether the policy uniquely matches at least one request (i.e. there is no other
-                // policy that matches the request). For "forbid" policies this considers only other
-                // "forbid" policies.
-                boolean uniquelyMatchesRequest;
-
-                // Whether it has been reported that this policy is subsumed by another (or if it
-                // does not match any requests)
-                boolean subsumptionReported;
-
-                // Policies which subsume or match an identical set of requests to this one
-                Set<Policy> subsumers;
-            }
-
-            // Create a map of policy to policy-related analysis results:
-            Map<Policy, PolicyAnalysisResult> analysisResults = new HashMap<>();
-            policyResults.keySet().forEach(p -> {
-                analysisResults.put(p, new PolicyAnalysisResult());
-            });
-
-            // Check for and report policies not matching any requests:
-            policyResults.forEach((k, v) -> {
-                if (v.isEmpty()) {
-                    Report r = new Report(Severity.Warning, "Policy '" + k.getName() + "' does not " + "match any requests", k.getSourceLoc());
-                    results.add(r);
-
-                    // A policy not matching any requests will be trivially subsumed by every other
-                    // policy; mark subsumption as reported to avoid further subsumption reports.
-                    analysisResults.get(k).subsumptionReported = true;
-                }
-            });
-
-            // Subsumption - which policies may be subsumed (or equal to) others. Start by assuming
-            // that all policies have an equal request set, i.e. that each policy may be subsumed by
-            // every other individual policy:
-            analysisResults.forEach((p, analysisResult) -> {
-                if (!analysisResult.subsumptionReported) {
-                    analysisResult.subsumers = new HashSet<>();
-                    analysisResult.subsumers.addAll(analysisResults.keySet());
-                    analysisResult.subsumers.remove(p);
-                }
-            });
-
-            // Now trim the subsumption sets by processing each request:
-            requestPolicyMap.forEach((k, v) -> {
-                // The set (v) of policies matching the request has policies which may subsume each
-                // other. But, any policy not in the set does not subsume any policy in the set:
-                v.forEach(p -> {
-                    Set<Policy> subsumersOfP = analysisResults.get(p).subsumers;
-                    subsumersOfP.removeIf(p2 -> !v.contains(p2));
-                });
-
-                if (v.size() == 1) {
-                    analysisResults.get(v.iterator().next()).uniquelyMatchesRequest = true;
-                } else {
-                    // If there is only a single forbid policy that matches, consider it a unique
-                    // match. (We don't want to report that a forbid policy does not uniquely match
-                    // requests just because those requests are otherwise permitted).
-                    Policy singleForbid = null;
-                    for (Policy p : v) {
-                        if (p.isForbid()) {
-                            if (singleForbid != null) {
-                                singleForbid = null;
-                                break;
-                            }
-                            singleForbid = p;
-                        }
-                    }
-
-                    if (singleForbid != null) {
-                        analysisResults.get(singleForbid).uniquelyMatchesRequest = true;
-                    }
-                }
-            });
-
-            // Create reports for subsumed policies
-            analysisResults.forEach((p, analysisResult) -> {
-                Set<Policy> ss = analysisResult.subsumers;
-                if (ss == null) {
-                    return;
-                }
-                ss.forEach(p2 -> {
-                    // (It is ok for a "forbid" policy to be "subsumed" by a "permit" policy - in that
-                    // case the forbid acts as a filter over the permit).
-                    if (!p.isForbid() || p2.isForbid()) {
-                        Report r;
-                        if (analysisResults.get(p2).subsumers.contains(p)) {
-                            // If two policies subsume each other, they match an identical set of
-                            // requests (this will be reported twice: once for each of the policies).
-                            r = new Report(Severity.Warning, "Policy '" + p.getName() + "' and '" + p2.getName() + "' match the same set of requests", p.getSourceLoc(), p2.getSourceLoc());
-                        } else {
-                            r = new Report(Severity.Warning, "Policy '" + p.getName() + "' does " + "not match any requests that are not also matched by policy '" + p2.getName() + "'", p.getSourceLoc(), p2.getSourceLoc());
-                        }
-                        analysisResults.get(p).subsumptionReported = true;
-                        results.add(r);
-                    }
-                    // TODO if a permit policy is subsumed by a forbid policy, make that clear in the
-                    //      report
-                });
-            });
-
-            // Create reports for policies that don't uniquely match any request
-            analysisResults.forEach((p, analysisResult) -> {
-                if (!analysisResult.uniquelyMatchesRequest && !analysisResult.subsumptionReported) {
-                    Report r = new Report(Severity.Warning, "Policy '" + p.getName() + "' does not " + "uniquely match any request", "Removing this policy would (by itself) have no effect", p.getSourceLoc());
-                    results.add(r);
-                }
-            });
-
-            // Check invariants and report any that don't hold
-            Map<Invariant, InvariantResult> invariantResults = translation.getInvariantResult();
-            invariantResults.forEach((k, v) -> {
-                if (!v.holds()) {
-                    Set<InvariantAssignment> assignments = v.getAssignments();
-                    String examples = "";
-                    if (!assignments.isEmpty()) {
-                        if (assignments.size() == 1) {
-                            examples = "Counterexample: " + assignments.iterator().next().toString();
-                        } else {
-                            examples = "Counterexamples: ";
-                            Iterator<InvariantAssignment> i = assignments.iterator();
-                            for (int count = 0; count < 3 && i.hasNext(); count++) {
-                                if (count != 0) {
-                                    examples += ", ";
-                                }
-                                examples += i.next().toString();
-                            }
-                            if (i.hasNext()) {
-                                examples += " (and " + (assignments.size() - 3) + " more)";
-                            }
-                        }
-                    }
-
-                    Report r = new Report(Severity.Error, "Invariant does not hold", examples, k.getSourceLoc());
-                    results.add(r);
-                }
-            });
-            return results;
-
-        } else {
+        if (TESTING) {
             return generateRandomReports(policies, schemas, entities, invariants);
         }
 
+        // TODO: use PolicyParser instead of PolicyProgram and pass file name separately
+        if (policies.isEmpty() || policies.iterator().next().isEmpty()) {
+            throw new ConfigurationException("No policies provided");
+        }
+
+        if (schemas.isEmpty()) {
+            throw new ConfigurationException("No schema provided");
+        }
+
+        if (entities.isEmpty()) {
+            throw new ConfigurationException("No entities provided");
+        }
+
+        Set<Report> results = new HashSet<>();
+
+        List<Pair<String, Path>> policyVersions = policies.iterator().next();
+        Path policiesPath = policyVersions.getLast().getValue();
+        Path schemaPath = schemas.values().iterator().next();
+        Path entitiesPath = entities.values().iterator().next();
+        Path invariantsPath = invariants.values().iterator().next();
+
+        Path dlPath = Files.createTempDirectory("rsvp-");
+
+        Translation translation = new Translation(schemaPath, policiesPath, entitiesPath, invariantsPath, dlPath);
+
+        Map<Policy, RequestSet> policyResults = translation.getPolicyResult();
+
+        // Generate the inverse map, request -> policy set:
+        Map<Request, Set<Policy>> requestPolicyMap = new HashMap<>();
+        policyResults.forEach((k, v) -> {
+            v.forEach(r -> {
+                requestPolicyMap.computeIfAbsent(r, y -> {
+                    return new HashSet<>();
+                }).add(k);
+            });
+        });
+
+        class PolicyAnalysisResult {
+            // Whether the policy uniquely matches at least one request (i.e. there is no other
+            // policy that matches the request). For "forbid" policies this considers only other
+            // "forbid" policies.
+            boolean uniquelyMatchesRequest;
+
+            // Whether it has been reported that this policy is subsumed by another (or if it
+            // does not match any requests)
+            boolean subsumptionReported;
+
+            // Policies which subsume or match an identical set of requests to this one
+            Set<Policy> subsumers;
+        }
+
+        // Create a map of policy to policy-related analysis results:
+        Map<Policy, PolicyAnalysisResult> analysisResults = new HashMap<>();
+        policyResults.keySet().forEach(p -> {
+            analysisResults.put(p, new PolicyAnalysisResult());
+        });
+
+        // Check for and report policies not matching any requests:
+        policyResults.forEach((k, v) -> {
+            if (v.isEmpty()) {
+                Report r = new Report(Severity.Warning, "Policy '" + k.getName() + "' does not " + "match any requests", k.getSourceLoc());
+                results.add(r);
+
+                // A policy not matching any requests will be trivially subsumed by every other
+                // policy; mark subsumption as reported to avoid further subsumption reports.
+                analysisResults.get(k).subsumptionReported = true;
+            }
+        });
+
+        // Subsumption - which policies may be subsumed (or equal to) others. Start by assuming
+        // that all policies have an equal request set, i.e. that each policy may be subsumed by
+        // every other individual policy:
+        analysisResults.forEach((p, analysisResult) -> {
+            if (!analysisResult.subsumptionReported) {
+                analysisResult.subsumers = new HashSet<>();
+                analysisResult.subsumers.addAll(analysisResults.keySet());
+                analysisResult.subsumers.remove(p);
+            }
+        });
+
+        // Now trim the subsumption sets by processing each request:
+        requestPolicyMap.forEach((k, v) -> {
+            // The set (v) of policies matching the request has policies which may subsume each
+            // other. But, any policy not in the set does not subsume any policy in the set:
+            v.forEach(p -> {
+                Set<Policy> subsumersOfP = analysisResults.get(p).subsumers;
+                subsumersOfP.removeIf(p2 -> !v.contains(p2));
+            });
+
+            if (v.size() == 1) {
+                analysisResults.get(v.iterator().next()).uniquelyMatchesRequest = true;
+            } else {
+                // If there is only a single forbid policy that matches, consider it a unique
+                // match. (We don't want to report that a forbid policy does not uniquely match
+                // requests just because those requests are otherwise permitted).
+                Policy singleForbid = null;
+                for (Policy p : v) {
+                    if (p.isForbid()) {
+                        if (singleForbid != null) {
+                            singleForbid = null;
+                            break;
+                        }
+                        singleForbid = p;
+                    }
+                }
+
+                if (singleForbid != null) {
+                    analysisResults.get(singleForbid).uniquelyMatchesRequest = true;
+                }
+            }
+        });
+
+        // Create reports for subsumed policies
+        analysisResults.forEach((p, analysisResult) -> {
+            Set<Policy> ss = analysisResult.subsumers;
+            if (ss == null) {
+                return;
+            }
+            ss.forEach(p2 -> {
+                // (It is ok for a "forbid" policy to be "subsumed" by a "permit" policy - in that
+                // case the forbid acts as a filter over the permit).
+                if (!p.isForbid() || p2.isForbid()) {
+                    Report r;
+                    if (analysisResults.get(p2).subsumers.contains(p)) {
+                        // If two policies subsume each other, they match an identical set of
+                        // requests (this will be reported twice: once for each of the policies).
+                        r = new Report(Severity.Warning, "Policy '" + p.getName() + "' and '" + p2.getName() + "' match the same set of requests", p.getSourceLoc(), p2.getSourceLoc());
+                    } else {
+                        r = new Report(Severity.Warning, "Policy '" + p.getName() + "' does " + "not match any requests that are not also matched by policy '" + p2.getName() + "'", p.getSourceLoc(), p2.getSourceLoc());
+                    }
+                    analysisResults.get(p).subsumptionReported = true;
+                    results.add(r);
+                }
+                // TODO if a permit policy is subsumed by a forbid policy, make that clear in the
+                //      report
+            });
+        });
+
+        // Create reports for policies that don't uniquely match any request
+        analysisResults.forEach((p, analysisResult) -> {
+            if (!analysisResult.uniquelyMatchesRequest && !analysisResult.subsumptionReported) {
+                Report r = new Report(Severity.Warning, "Policy '" + p.getName() + "' does not " + "uniquely match any request", "Removing this policy would (by itself) have no effect", p.getSourceLoc());
+                results.add(r);
+            }
+        });
+
+        // Check invariants and report any that don't hold
+        Map<Invariant, InvariantResult> invariantResults = translation.getInvariantResult();
+        invariantResults.forEach((k, v) -> {
+            if (!v.holds()) {
+                Set<InvariantAssignment> assignments = v.getAssignments();
+                String examples = "";
+                if (!assignments.isEmpty()) {
+                    if (assignments.size() == 1) {
+                        examples = "Counterexample: " + assignments.iterator().next().toString();
+                    } else {
+                        examples = "Counterexamples: ";
+                        Iterator<InvariantAssignment> i = assignments.iterator();
+                        for (int count = 0; count < 3 && i.hasNext(); count++) {
+                            if (count != 0) {
+                                examples += ", ";
+                            }
+                            examples += i.next().toString();
+                        }
+                        if (i.hasNext()) {
+                            examples += " (and " + (assignments.size() - 3) + " more)";
+                        }
+                    }
+                }
+
+                Report r = new Report(Severity.Error, "Invariant does not hold", examples, k.getSourceLoc());
+                results.add(r);
+            }
+        });
+        return results;
     }
 
 

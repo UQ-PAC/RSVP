88cbdd22382a606adf9c223c92e19ddfc972ee20
nunesgh <contact@nunesgh.com>
2026-05-25 17:43:24 +1000

    License header management for the ChildrenClinic.

--- childrenclinic/app/src/main/java/uq/pac/childrenclinic/model/BaseEntity.java
+++ childrenclinic/app/src/main/java/uq/pac/childrenclinic/model/BaseEntity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2012-2025 the original author or authors.
+ * Copyright 2026 Gabriel Henrique Lopes Gomes Alves Nunes.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +13,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file has been modified from the original Spring PetClinic project
+ * (https://github.com/spring-projects/spring-petclinic).
  */
+
 package uq.pac.childrenclinic.model;
 
 import jakarta.persistence.Column;

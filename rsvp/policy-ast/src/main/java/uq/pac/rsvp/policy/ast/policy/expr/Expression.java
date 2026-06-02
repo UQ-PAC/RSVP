82bd4f32622202fd531335707bb7760c90a2a98f
Kostyantyn Vorobyov <k.vorobyov@uq.edu.au>
2026-06-02 14:43:15 +1000

    policy-dl: normal form class

--- rsvp/policy-ast/src/main/java/uq/pac/rsvp/policy/ast/policy/expr/Expression.java
+++ rsvp/policy-ast/src/main/java/uq/pac/rsvp/policy/ast/policy/expr/Expression.java
@@ -2,6 +2,7 @@ package uq.pac.rsvp.policy.ast.policy.expr;
 
 import java.util.regex.Pattern;
 import uq.pac.rsvp.policy.ast.policy.PolicyAstNode;
+import uq.pac.rsvp.policy.ast.policy.PolicyParser;
 import uq.pac.rsvp.support.SourceLoc;
 
 public abstract class Expression extends PolicyAstNode {
@@ -12,4 +13,7 @@ public abstract class Expression extends PolicyAstNode {
         super(source);
     }
 
+    public static Expression parse(String expr) {
+        return PolicyParser.parse(expr);
+    }
 }
--- rsvp/policy-dl/src/main/java/uq/pac/rsvp/policy/datalog/translation/NormalForm.java
+++ rsvp/policy-dl/src/main/java/uq/pac/rsvp/policy/datalog/translation/NormalForm.java
@@ -6,20 +6,36 @@ import static uq.pac.rsvp.policy.ast.policy.expr.BinaryExpression.BinaryOp.And;
 import static uq.pac.rsvp.policy.ast.policy.expr.BinaryExpression.BinaryOp.Or;
 import static uq.pac.rsvp.policy.ast.policy.expr.UnaryExpression.UnaryOp.Not;
 
-public class
+/**
+ * Converting expressions to a normal form, such as NNF or CND
+ */
+public class NormalForm {
 
-public class NNFTransformer extends ExpressionAdapter {
-    public Expression visitUnaryExpr(UnaryExpression expr) {
-        if (expr.getOp() == Not && expr.getExpression() instanceof BinaryExpression bin &&
-                (bin.getOp() == Or || bin.getOp() == And)) {
-            BinaryExpression.BinaryOp op = bin.getOp() == And ? Or : And;
+    static class NNFTransformer extends ExpressionAdapter {
+        private static final NNFTransformer TRANSFORMER = new NNFTransformer();
+
+        private NNFTransformer() { }
+
+        Expression transform(Expression expr) {
+            return expr.compute(TRANSFORMER);
+        }
+
+        public Expression visitUnaryExpr(UnaryExpression expr) {
+            if (expr.getOp() == Not && expr.getExpression() instanceof BinaryExpression bin &&
+                    (bin.getOp() == Or || bin.getOp() == And)) {
+                BinaryExpression.BinaryOp op = bin.getOp() == And ? Or : And;
                 Expression lhs = bin.getLeft().compute(this),
                         rhs = bin.getRight().compute(this);
                 return new BinaryExpression(new UnaryExpression(Not, lhs), op, new UnaryExpression(Not, rhs));
-        } else {
-            return new UnaryExpression(expr.getOp(),
-                    expr.getExpression().compute(this),
-                    expr.getSourceLoc());
+            } else {
+                return new UnaryExpression(expr.getOp(),
+                        expr.getExpression().compute(this),
+                        expr.getSourceLoc());
+            }
         }
+    }
+
+    static class CNFTransformer extends ExpressionAdapter {
+
     }
 }
